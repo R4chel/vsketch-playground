22b08ffc5ba015099a82aad06a142bf2f0aae07c
R4chel <rae84@cornell.edu>
2022-07-13 13:19:21 -0400

    add a circle

--- first_project/sketch_first_project.py
+++ first_project/sketch_first_project.py
@@ -3,14 +3,16 @@ import vsketch
 
 class FirstProjectSketch(vsketch.SketchClass):
     # Sketch parameters:
-    # radius = vsketch.Param(2.0)
+    min_radius = vsketch.Param(2.0)
+    max_radius = vsketch.Param(10.0)
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a4", landscape=True)
         vsk.scale("cm")
 
         # implement your sketch here
-        # vsk.circle(0, 0, self.radius, mode="radius")
+
+        vsk.circle(0, 0, vsk.random(self.min_radius,self.max_radius), mode="radius")
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
