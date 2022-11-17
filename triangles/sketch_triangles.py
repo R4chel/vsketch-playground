128dab9cba7ded7a998338d8c01849bfe6c2dec9
R4chel <rae84@cornell.edu>
2022-11-17 16:34:48 -0500

    trying to debug

--- triangles/sketch_triangles.py
+++ triangles/sketch_triangles.py
@@ -19,17 +19,25 @@ class TrianglesSketch(vsketch.SketchClass):
         xmax = vsk.width
         ymin = 0
         ymax = vsk.height
-        vsk.rect(0,0,vsk.width, vsk.height, mode="corner")
-        p0 = Point(vsk.random(xmax), vsk.random(ymax))
 
-        vsk.rect(0,0,vsk.width, vsk.height, mode="corner")
+        p0 = Point(vsk.random(xmax), vsk.random(ymax))
         angle_delta = math.pi * (1.0 - self.angle_ish_N/self.angle_ish_D )
         angle = vsk.random(0, 2 * math.pi)
         step_size = self.step_increment
 
+    
+        vsk.circle(559.3700787401575, 396.85039370078744, 20)
+        vsk.circle(vsk.width, vsk.height, 30)
         for i in range(self.steps):
             p1 = Point(p0.x + step_size*math.cos(angle), p0.y + step_size*math.sin(angle))
             vsk.line(p0.x, p0.y,p1.x,p1.y)
+            if p1.x > vsk.width or p1.x < 0 or p1.y < 0 or p1.y > vsk.height:
+            
+                print ("here", i)
+                print(vsk.width, vsk.height)
+                print (p0)
+                print (p1)
+                # break;
             step_size += self.step_increment
             angle += angle_delta
             p0 = p1
